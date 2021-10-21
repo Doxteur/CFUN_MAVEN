911b27b138602f85942043f51354c055f85ec3bf
PierreWZK <pierredolivet@live.fr>
2021-10-21 10:29:21 +0200

    Debut BarCode

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,8 @@ module com.CFUN.CFUNGIT {
 	requires java.desktop;
 	requires javafx.graphics;
 	requires javafx.base;
+	requires com.google.zxing.javase;
+	requires com.google.zxing;
 	
     opens com.CFUN.CFUNGIT to javafx.fxml;
     exports com.CFUN.CFUNGIT;
